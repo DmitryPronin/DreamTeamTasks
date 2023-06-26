a2ace5cb899dae1ff733bad43c4b63a48356f642
Дмитрий <dmit.pronin@gmail.com>
2023-06-26 22:28:48 +0300

    решение задачи с пропущенным уникальным числом

--- src/main/java/randommisseddigit/RandomMissedDigit.java
+++ src/main/java/randommisseddigit/RandomMissedDigit.java
@@ -1,24 +1,20 @@
 package randommisseddigit;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Random;
-import java.util.Set;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class RandomMissedDigit {
     public static void main(String[] args) {
         Random random = new Random();
         HashSet<Integer> integers = new HashSet<>();
 
-        while(integers.size() < 1000){
+        while (integers.size() < 1000) {
             int randomInt = random.nextInt(1000) + 1;
             System.out.println("\"" + randomInt + "\"");
             integers.add(randomInt);
         }
 
-        for (int i = 1 ; i <= 1000; i++) {
+        for (int i = 1; i <= 1000; i++) {
             if (!integers.contains(i)) {
                 System.out.println("Пропущено число " + i);
                 System.exit(0);
